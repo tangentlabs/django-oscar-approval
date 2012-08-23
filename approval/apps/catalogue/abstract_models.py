e8b8b99fd84e26e546bc8a011881040d14733cef
Asia Biega <asia.biega@gmail.com>
2012-08-23 12:42:12 +0200

    Skeleton, setup, product model extension

---
+++ approval/apps/catalogue/abstract_models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class AbstractProduct(models.Model):
+
+    requires_approval = models.BooleanField(_("Requires authorisation?"), default=False)
+
+    class Meta:
+        abstract = True
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='django-oscar-approval',
+      version='0.0.1',
+      url='https://github.com/tangentlabs/django-oscar-approval',
+      author="Tangentlabs",
+      author_email="",
+      description="Order approval module for django-oscar",
+      keywords="order approval, order authorisation",
+      license='BSD',
+      packages=['approval',],
+      include_package_data=True,
+      classifiers=['Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python']
+      )
