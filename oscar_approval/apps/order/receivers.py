be318a139876e18c2cc985cd19bbfac1873bc12a
Asia Biega <asia.biega@gmail.com>
2012-08-23 17:38:38 +0200

    Receiver for order_placed that sets initial authorisation status

---
+++ oscar_approval/apps/order/receivers.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+def receive_order_placed(sender, order, user, **kwargs):
+    authorisation_required = False
+    for line in order.lines.all():
+        if line.product.requires_approval:
+            line.status = settings.OSCAR_LINE_APPROVAL_STATUS
+            line.save()
+            authorisation_required = True
+
+    if authorisation_required:
+        order.status = settings.OSCAR_ORDER_APPROVAL_STATUS
+        order.save()
--- oscar_approval/apps/order/utils.py
+++ oscar_approval/apps/order/utils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class OrderCreatorApprovalMixin(object):
 
-    def update_line_approval_extra_fields(self, basket_line, extra_line_fields):
+    def update_line_approval_extra_fields(self, order, basket_line, extra_line_fields):
 
         """
             Updates line additional data with stuff related to order approval.
@@ -16,5 +16,7 @@ class OrderCreatorApprovalMixin(object):
             if extra_line_fields is None:
                 extra_line_fields = {}
         extra_line_fields['status'] = settings.OSCAR_LINE_APPROVAL_STATUS
+        order.status = settings.OSCAR_ORDER_APPROVAL_STATUS
+        order.save()
         return extra_line_fields
 
