6fc6eaeb2662f43ea4e1e67c07abb731eff2d2ad
Asia Biega <asia.biega@gmail.com>
2012-08-24 17:44:16 +0200

    Line approval and rejection.
    Added line_approved and line_rejected signals. Simple views for handling authorisation actions. Simple pending items list template.

--- oscar_approval/apps/approval/views.py
+++ oscar_approval/apps/approval/views.py
@@ -1,10 +1,47 @@
 from django.views import generic
 from django.db.models import get_model
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from . import signals
 
 Line = get_model('order', 'Line')
 
 
-class OrderLineApprovalList(generic.ListView):
+class OrderLineApprovalListView(generic.ListView):
 
     template_name = 'oscar_approval/order_line_approval_list.html'
     model = Line
+
+    def get_queryset(self):
+        return self.model.objects.filter(
+                    status=settings.OSCAR_LINE_APPROVAL_STATUS)
+
+
+class OrderLineApproveView(generic.UpdateView):
+
+    model = Line
+
+    def post(self, request, *args, **kwargs):
+        signals.order_line_approved.send(sender=self,
+                                         line=self.get_object(),
+                                         user=request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('order-line-approval-list')
+
+
+class OrderLineRejectView(generic.UpdateView):
+
+    model = Line
+
+    def post(self, request, *args, **kwargs):
+        signals.order_line_rejected.send(sender=self,
+                                         line=self.get_object(),
+                                         user=request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('order-line-approval-list')
--- oscar_approval/urls.py
+++ oscar_approval/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls.defaults import patterns, url
 from apps.approval import views
 
 urlpatterns = patterns('',
-    url(r'^list/order-line/', views.OrderLineApprovalList.as_view(), 
+    url(r'^order-line/list/$', views.OrderLineApprovalListView.as_view(),
         name='order-line-approval-list'),
+    url(r'^order-line/(?P<pk>\d+)/approve/$', views.OrderLineApproveView.as_view(),
+        name='order-line-approve'),
+    url(r'^order-line/(?P<pk>\d+)/reject/$', views.OrderLineRejectView.as_view(),
+        name='order-line-reject'),
 )
