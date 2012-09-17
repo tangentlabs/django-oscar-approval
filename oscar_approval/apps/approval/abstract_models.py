53b91bd38b5b05e9745ee881bf8cd59989055bcf
Asia Biega <asia.biega@gmail.com>
2012-09-17 12:48:46 +0200

    Larger limit for log event type length

--- oscar_approval/apps/approval/abstract_models.py
+++ oscar_approval/apps/approval/abstract_models.py
@@ -16,7 +16,7 @@ class AbstractOrderLineApprovalLog(models.Model):
     line = models.ForeignKey('order.Line')
     user = models.ForeignKey(User)
     event_date = models.DateTimeField(auto_now_add=True)
-    event_type = models.CharField(max_length=20)
+    event_type = models.CharField(max_length=30)
 
     class Meta:
         abstract = True
