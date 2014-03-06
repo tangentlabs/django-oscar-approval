1007b24207cb3462dd2d62279de227ecad743946
Izidor Matušov <izidor.matusov@gmail.com>
2014-03-06 13:11:10 +0000

    Use relative imports

--- oscar_approval/apps/approval/models.py
+++ oscar_approval/apps/approval/models.py
@@ -5,12 +5,8 @@ class OrderLineApprovalLog(AbstractOrderLineApprovalLog):
 
     pass
 
-
-
-from oscar_approval.apps.approval.signals import (order_line_approved,
-                                                  order_line_rejected)
-from oscar_approval.apps.approval.receivers import (receive_order_line_approved,
-                                                    receive_order_line_rejected)
+from .signals import order_line_approved, order_line_rejected
+from .receivers import receive_order_line_approved, receive_order_line_rejected
 
 order_line_approved.connect(receive_order_line_approved)
 order_line_rejected.connect(receive_order_line_rejected)
