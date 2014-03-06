fe737847d3265b0e44a9c84f363031ca1ded3554
Izidor Matušov <izidor.matusov@gmail.com>
2014-03-06 13:10:56 +0000

    Include all required files

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='django-oscar-approval',
-      version='0.1',
-      url='https://github.com/tangentlabs/django-oscar-approval',
-      author="Tangentlabs",
-      author_email="",
-      description="Order approval module for django-oscar",
-      keywords="order approval, order authorisation",
-      license='BSD',
-      packages=['oscar_approval'],
-      include_package_data=True,
-      classifiers=['Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: Unix',
-                   'Programming Language :: Python'],
-      install_requires=[
-          'django-oscar>=0.4,<0.6',
-      ])
+setup(
+    name='django-oscar-approval',
+    version='0.1',
+    url='https://github.com/tangentlabs/django-oscar-approval',
+    author="Tangentlabs",
+    author_email="tangentlabs@tangentlabs.co.uk",
+    description="Order approval module for django-oscar",
+    license='BSD',
+    keywords="order approval, order authorisation",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+    ],
+    install_requires=[
+        'django-oscar>=0.4,<0.6',
+    ])
